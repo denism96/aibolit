a5008b4d8dcc0edd09c8362a050ae0055a98c145
denism96 <drag33546@bk.ru>
2024-10-11 00:08:39 +0300

    init + config tailwind + scss

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <img src="/src/assets/image/logo.svg" alt="logo" />
+    <nav>
+      <ul class="header-list">
+        <li class="header-list-item">ЛИЧНЫЙ КАБИНЕТ</li>
+        <li class="header-list-item">О НАС</li>
+        <li class="header-list-item">НОВОСТИ И АКЦИИ</li>
+        <li class="header-list-item">КОНТАКТЫ</li>
+        <li class="header-list-item">БЛОГ</li>
+      </ul>
+    </nav>
+    <span>Волгоград</span>
+  </header>
+</template>
+<style lang="scss" scoped></style>
