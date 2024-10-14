3ee3750f6bc728b6ac00af0e0469448349e6c398
denism96 <drag33546@bk.ru>
2024-10-15 00:31:28 +0300

    create Tabs component

---
+++ src/components/AppTabs.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  names: {
+    type: Array,
+    required: true,
+  },
+  selectedTab: {
+    type: String,
+    required: true,
+  },
+});
+const emit = defineEmits(["changeTab"]);
+
+const clickOnTab = (tabName) => {
+  emit("changeTab", tabName);
+};
+</script>
+<template>
+  <div class="tab-nav">
+    <span
+      v-for="tab in names"
+      :key="tab.name"
+      :class="['tab-nav__item', { selected: tab.name === selectedTab }]"
+      @click="clickOnTab(tab.name)"
+      >{{ tab.name }}
+    </span>
+  </div>
+  <div class="tab-content"><slot /></div>
+</template>
+<style lang="scss" scoped>
+span {
+  margin-right: 30px;
+  margin-bottom: 20px;
+  display: block;
+  width: 100px;
+  height: 30px;
+  background-color: red;
+  cursor: pointer;
+}
+.selected {
+  background-color: blue;
+}
+</style>
--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,17 +1,68 @@
 <script setup lang="ts">
 import SocialLink from "./social/SocialLink.vue";
+import AppTabs from "./AppTabs.vue";
+import { ref } from "vue";
+const tabs = [
+  {
+    name: "1",
+    label: "Айболит круглосуточная ветклиника",
+    image: "/src/assets/image/tabs/cat_dog.png",
+    imageAlt: "Кот с собакой",
+  },
+  {
+    name: "2",
+    label: "Комфортные оборудованные стационары",
+    image: "/src/assets/image/tabs/doctor.png",
+    imageAlt: "Собака облизывает доктора",
+  },
+  {
+    name: "3",
+    label: "Новейшее европейское оборудование",
+    image: "/src/assets/image/tabs/dog_doctor.png",
+    imageAlt: "Собака доктор",
+  },
+  {
+    name: "4",
+    label: "Собственная современная лаборатория",
+    image: "/src/assets/image/tabs/rabbit.png",
+    imageAlt: "Кролик",
+  },
+  {
+    name: "5",
+    label: "Аптека и зоомагазин по доступным ценам ",
+    image: "/src/assets/image/tabs/cat_with_tongue.png",
+    imageAlt: "Кошка показывает язык",
+  },
+  {
+    name: "6",
+    label: "Груминг салон и косметические товары",
+    image: "/src/assets/image/tabs/dogs.png",
+    imageAlt: "Собаки той терьеры",
+  },
+];
+
+const selectedTab = ref(tabs[0].name);
+
+const changeTab = (tabName) => {
+  selectedTab.value = tabName;
+};
 </script>
 
 <template>
   <section>
+    <AppTabs :names="tabs" :selectedTab="selectedTab" @changeTab="changeTab">
+    </AppTabs>
     <div class="center-content">
-      <h1>Айболит круглосуточная ветклиника</h1>
+      <div v-for="tab in tabs" :key="tab.name">
+        <h1 v-if="selectedTab === tab.name">{{ tab.label }}</h1>
+        <div v-if="selectedTab === tab.name">
+          <img class="dog-image" :src="tab.image" :alt="tab.imageAlt" />
+        </div>
+      </div>
       <button>Записаться</button>
       <SocialLink></SocialLink>
     </div>
     <div class="right-content">
-      <img class="cat-image" src="/src/assets/image/cat.png" alt="cat" />
-      <img class="dog-image" src="/src/assets/image/dog.png" alt="dog" />
       <p>8 121 909 00 00</p>
     </div>
   </section>
