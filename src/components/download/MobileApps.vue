2eb77add221bc9ffca9b13c6eddb20a0d963d7d6
denism96 <drag33546@bk.ru>
2024-10-14 22:19:00 +0300

    Collecting repeating elements into 1 component

---
+++ src/components/download/MobileApps.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="apps">
+    <img class="appstore" src="/src/assets/image/mobile/appstore.png" alt="" />
+    <img
+      class="googleplay"
+      src="/src/assets/image/mobile/googleplay.png"
+      alt=""
+    />
+  </div>
+</template>
+<style lang="scss" scoped>
+.apps {
+  display: flex;
+}
+.appstore {
+  margin-top: 12px;
+  height: 49px;
+}
+</style>
---
+++ src/components/social/SocialLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="social">
+    <img src="/src/assets/image/inst.svg" alt="inst" />
+    <img src="/src/assets/image/fb.svg" alt="fb" />
+    <img src="/src/assets/image/vk.svg" alt="vk" />
+    <img src="/src/assets/image/youtube.svg" alt="youtube" />
+  </div>
+</template>
+<style lang="scss" scoped>
+.social {
+  display: flex;
+  justify-content: space-between;
+  margin-top: auto;
+  margin-bottom: 50px;
+  width: 340px;
+}
+</style>
