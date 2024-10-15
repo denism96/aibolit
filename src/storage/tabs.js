79207f83cc36b52705b860b2a3f225a49097d299
denism96 <drag33546@bk.ru>
2024-10-16 02:19:04 +0300

    DB tabs & catalogCards

---
+++ src/storage/tabs.js
@@ -0,0 +1,38 @@
+export const tabs = [
+    {
+      name: "1",
+      label: "Айболит круглосуточная ветклиника",
+      image: "/src/assets/image/tabs/cat_dog.png",
+      imageAlt: "Кот с собакой",
+    },
+    {
+      name: "2",
+      label: "Комфортные оборудованные стационары",
+      image: "/src/assets/image/tabs/doctor.png",
+      imageAlt: "Собака облизывает доктора",
+    },
+    {
+      name: "3",
+      label: "Новейшее европейское оборудование",
+      image: "/src/assets/image/tabs/dog_doctor.png",
+      imageAlt: "Собака доктор",
+    },
+    {
+      name: "4",
+      label: "Собственная современная лаборатория",
+      image: "/src/assets/image/tabs/rabbit.png",
+      imageAlt: "Кролик",
+    },
+    {
+      name: "5",
+      label: "Аптека и зоомагазин по доступным ценам ",
+      image: "/src/assets/image/tabs/cat_with_tongue.png",
+      imageAlt: "Кошка показывает язык",
+    },
+    {
+      name: "6",
+      label: "Груминг салон и косметические товары",
+      image: "/src/assets/image/tabs/dogs.png",
+      imageAlt: "Собаки той терьеры",
+    },
+  ];
