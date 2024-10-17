6e66f91238b7fee24ceb5288416482c9576d81e9
denism96 <drag33546@bk.ru>
2024-10-17 18:54:52 +0300

    update tabs

--- src/components/AppTabs.vue
+++ src/components/AppTabs.vue
@@ -16,29 +16,32 @@ const clickOnTab = (tabName) => {
 };
 </script>
 <template>
-  <div class="tab-nav">
-    <span
+  <ul class="tab-nav">
+    <li
       v-for="tab in names"
       :key="tab.name"
       :class="['tab-nav__item', { selected: tab.name === selectedTab }]"
       @click="clickOnTab(tab.name)"
-      >{{ tab.name }}
-    </span>
-  </div>
-  <div class="tab-content"><slot /></div>
+    >
+      {{ tab.name }}
+    </li>
+  </ul>
 </template>
 <style lang="scss" scoped>
 .tab-nav {
+  position: absolute;
+  top: 0;
+  left: 0;
   display: flex;
   flex-direction: column;
+  width: 30px;
 }
-span {
-  margin-right: 30px;
-  margin-bottom: 20px;
-  padding: 3px 10px;
-  cursor: pointer;
+li {
   text-align: center;
+  margin-bottom: 20px;
+  cursor: pointer;
   border-radius: 20px;
+  height: 30px;
 }
 .selected {
   border: 2px solid #000;
