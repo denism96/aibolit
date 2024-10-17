ede772084a35b3c2a40d3b54998dd0298b0b146b
denism96 <drag33546@bk.ru>
2024-10-17 18:57:11 +0300

    Update CatalogApp.vue

--- src/components/CatalogApp.vue
+++ src/components/CatalogApp.vue
@@ -1,20 +1,21 @@
 <script setup>
-import { cardsCatalog } from '../storage/cardsCatalog';
-import CatalogArticle from './CatalogArticle.vue';
+import { cardsCatalog } from "../storage/cardsCatalog";
+import CatalogArticle from "./CatalogArticle.vue";
 </script>
 <template>
-<section class="catalog " >
+  <section class="catalog">
     <CatalogArticle :cards="cardsCatalog"></CatalogArticle>
-</section>
+  </section>
 </template>
 <style lang="scss" scoped>
-@import '/src/assets/styles/main.scss';
+@import "/src/assets/styles/main.scss";
 .catalog {
-    display: flex;
-    width: 1305px;
-    flex-wrap: wrap;
-    margin: 0 auto;
-    justify-content: space-between;
-    margin-top: 50px;
+  display: flex;
+  width: 1305px;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  justify-content: space-between;
+  margin-top: 50px;
+  padding-bottom: 90px;
 }
 </style>
