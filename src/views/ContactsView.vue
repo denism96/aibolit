c9ed6f99838e02f1fce8d309c1b2c708eee70163
denism96 <drag33546@bk.ru>
2024-10-19 16:24:26 +0300

    Add contact component in ContactView

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,3 +1,13 @@
 <template>
-  <div class="contacts">Контакты</div>
+  <div class="content">
+    <SectionContacts></SectionContacts>
+  </div>
 </template>
+<script setup>
+import SectionContacts from "../components/home_page/SectionContacts.vue";
+</script>
+<style lang="scss" scoped>
+.content {
+  margin: 150px 0 150px 0;
+}
+</style>
